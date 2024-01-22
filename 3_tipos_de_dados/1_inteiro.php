d504d0acd81609d0dd729ac009bb4860a492e541
MarcoUpe <marcojufra@gmail.com>
2024-01-22 16:47:20 -0300

    mod3

---
+++ 3_tipos_de_dados/1_inteiro.php
@@ -0,0 +1,22 @@
+<?php
+
+    echo 5;
+    echo "<br>";
+    echo 5 + 7;
+    echo "<br>";
+    echo 7;
+    echo -12;
+    echo 7 - 12;
+    
+    $n = 90;
+    $n1 = 10;
+    $n2 = 20;
+
+    $soma = $n1 + $n2;
+
+    echo "<br>";
+    echo $n;
+    echo "<br>";
+    echo $soma;
+
+?>
---
+++ 3_tipos_de_dados/2_exercicio.php
@@ -0,0 +1,20 @@
+<?php
+
+    $num1 = 25;
+    $num2 = 30;
+    $num3 = 10;
+
+    $soma = $num1 + $num2 + $num3;
+    $media = ($num1 + $num2 + $num3)/3;
+
+    echo $num1;
+    echo "<br>";
+    echo $num2;
+    echo "<br>";
+    echo $num3;
+    echo "<br>";
+    echo $soma;
+    echo "<br>";
+    echo $media;
+
+?>

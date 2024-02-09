3ebfac9b1d84a2b8abf7b2d9c5b80518bf72c8c9
MarcoUpe <marcojufra@gmail.com>
2024-02-09 15:51:01 -0300

    Mod5

---
+++ 5_op_expressoes/2_exercicio_10/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    $a = 10;
+    $b = 20;
+    $c = 30;
+
+    $sub = $c - $b - $a;
+
+    echo "Sub: $sub <br>";
+
+    $div = $c / $a;
+
+    echo "Div: $div <br>";
+
+    $mult = $a * $b * $c;
+
+    echo "Mult: $mult <br>";
+
+?>

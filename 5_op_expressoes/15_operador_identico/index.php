e3f7f0c099e2c905cc03fd0b971b0afecc57cdfc
MarcoUpe <marcojufra@gmail.com>
2024-02-20 17:00:10 -0300

    OP_ID

--- 5_op_expressoes/15_operador_identico/index.php
+++ 5_op_expressoes/15_operador_identico/index.php
@@ -1,7 +1,34 @@
 <?php
 
-if (5 == "5"){
-    echo "5 é igual a 5 <br>";
-}
+    if (5 == "5"){
+        echo "5 é igual a 5  - 1 <br>";
+    }
+
+    //Op Identico
+    if (5 === "5"){
+        echo "5 é igual a 5 - 2 <br>";
+    }
+
+    //Op e valor identico
+    if (5 === 5){
+        echo "5 é igual a 5 - 3 <br>";
+    }
+    
+    if (5 === "Teste"){
+        echo "5 é igual a 5 - 4 <br>";
+    }
+
+    $a = 4;
+    $b = 4;
+    $c = "4";
+
+    if ($a === $b){
+        echo "A é igual A 1 <br>";
+    }
+
+    if ($a === $c){
+        echo "A é igual C 1 <br>";
+    }
+
 
 ?>

29fd685428112d8203c5de40375b6c79ca52b500
MarcoUpe <marcojufra@gmail.com>
2024-02-21 16:48:00 -0300

    1245

---
+++ 5_op_expressoes/20_menor_ou_igual_e_menor/index.php
@@ -0,0 +1,30 @@
+<?php
+
+    if(4 < 12) {
+        echo "4 eh menor que 12 <br>";
+    }
+
+    if(24 < 12) {
+        echo "24 eh menor que 12 <br>";
+    }
+
+    if(12 <= 12) {
+        echo "12 eh menor que 12 <br>";
+    }
+
+    $a = 5;
+    $b = 18;
+
+    if($a < $b) {
+        echo "A eh menor que B <br>";
+    }
+
+    if($b < $a) {
+        echo "B eh menor que A <br>";
+    }
+
+    if($b < 42) {
+        echo "A eh menor que B <br>";
+    }
+    
+?>

18e44b6b75660ab89643538affcb44419badf86e
MarcoUpe <marcojufra@gmail.com>
2024-03-07 16:53:15 -0300

    SEC_8_0703

---
+++ 9_funcoes/2_chamando_funcoes/index.php
@@ -0,0 +1,19 @@
+<?php
+    
+    // definindo funcao
+    function soma() {
+        
+        $a = 5;
+        $b = 4;
+
+        echo $a + $b . "<br>";
+
+    }
+
+    // chamando/invocando funcao
+    soma();
+
+    // funcao do PHP - MAISCULO
+    echo strtoupper("testando funcao php");
+
+?>

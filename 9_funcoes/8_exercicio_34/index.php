616cf2c9f211c607723d9f31887fec9be4487ac0
MarcoUpe <marcojufra@gmail.com>
2024-03-12 16:46:12 -0300

    sec_9_1203

--- 9_funcoes/8_exercicio_34/index.php
+++ 9_funcoes/8_exercicio_34/index.php
@@ -1,5 +1,14 @@
 <?php
 
-    
+    function imparPar($num) {
+        if ($num % 2 == 0) {
+            echo "$num eh par <br>";
+        } else {
+            echo "$num eh impar <br>";
+        }
+    }
+
+    imparPar(5);
+    imparPar(10);
 
 ?>

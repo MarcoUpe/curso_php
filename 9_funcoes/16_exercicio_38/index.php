55b77f260f88dcd961f02935b49920ef1289d690
MarcoUpe <marcojufra@gmail.com>
2024-03-20 16:50:53 -0300

    sec_9_2003

--- 9_funcoes/16_exercicio_38/index.php
+++ 9_funcoes/16_exercicio_38/index.php
@@ -2,8 +2,23 @@
 
     $lista = ["Arroz", "Trigo", "Feijão", "Cerveja", "Sal", "Vinagre"];
 
-    function listaParaString () {
+    function listaParaString ($arr) {
         
+        $str = "Você levou esses itens do mercado: ";
+
+        for ($i = 0; $i < count($arr); $i++) {
+            
+            if ($i + 1 == count($arr)) { // vai para ultimo argumento do array
+                $str .= "$arr[$i]. "; 
+            }
+            else {
+                $str .= "$arr[$i], "; // concatena str com array
+            }
+
+        }
+        return $str;
     }
 
+    echo listaParaString($lista);
+    
 ?>
---
+++ 9_funcoes/17_multiplos_retornos/index.php
@@ -0,0 +1,8 @@
+<?php
+
+    function alteraDados($nome, $idade) {
+
+        
+    }
+
+?>

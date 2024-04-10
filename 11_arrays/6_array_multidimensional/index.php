c99c9bfb10bdf4949a35039b013a5fbc5cbaf93d
MarcoUpe <marcojufra@gmail.com>
2024-04-10 16:58:05 -0300

    sec_11_1004

--- 11_arrays/6_array_multidimensional/index.php
+++ 11_arrays/6_array_multidimensional/index.php
@@ -8,6 +8,10 @@
     print_r($arr);
     echo "<br>";
 
-    
+    echo $arr[0][1] . '<br>'; // acessando primeiro array, e segundo elemento
+    echo $arr[1][2] . '<br>'; // acessando segundo array, e ultimo elemento
+
+    echo count($arr) . "<br>"; // count do array, são dois array internos
+    echo count($arr[0]) . "<br>"; // count do primeiro array com tres elementos
 
 ?>
---
+++ 11_arrays/8_funcao_list/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    $pessoa = ["Marco", 29, "Programador", "Castanho"];
+
+    print_r($pessoa);
+    echo "<br>";
+
+    list($nome, $idade, $profissao, $corDosOlhos) = $pessoa;
+
+    echo "$nome <br>";
+    echo "$idade <br>";
+    echo "$profissao <br>";
+    echo "$corDosOlhos <br>";
+
+?>

b7026ec290b890f671d8ac1f9ae7d198fbb6b0e2
MarcoUpe <marcojufra@gmail.com>
2024-05-20 16:54:50 -0300

    sec_alone

--- 11_arrays/26_array_reverse/index.php
+++ 11_arrays/26_array_reverse/index.php
@@ -2,7 +2,7 @@
 
     $arr = [1, 2, 3, 4, 5];
 
-    $arrRev = array_reverse($arr);
+    $arrRev = array_reverse($arr); // reverte a ordem dos elementos do array
 
     print_r($arr);
     echo "<br>";
@@ -13,7 +13,7 @@
 
     $arr2 = ["Marco", 12, true, [1, 2]];
 
-    $arr2Rev = array_reverse($arr2);
+    $arr2Rev = array_reverse($arr2); // reverte quaiquer que sejam os elementos
     print_r($arr2);
     echo "<br>";
     print_r($arr2Rev);

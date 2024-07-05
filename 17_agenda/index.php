2411f90679e29997e357f9acfd78ed93989dc45f
MarcoUPe <marcojufra@gmail.com>
2024-07-05 15:13:10 -0300

    sec_17_0407

--- 17_agenda/index.php
+++ 17_agenda/index.php
@@ -8,7 +8,25 @@
         <?php endif; ?>
         <h1 id="main-title">Minha Agenda</h1>
         <?php if(count($contacts) > 0): ?>
-            <p>TEM CONTATOS</p>
+            <table class="table" id="contacts-table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Telefone</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($contacts as $contacts): ?>
+                        <tr>
+                            <td scope="row"> <?php $contacts["id"] ?></td>
+                            <td scope="row"> <?php $contacts["name"] ?></td>
+                            <td scope="row"> <?php $contacts["phone"] ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
         <?php else: ?>
             <p id="empty-list-text">Ainda não há contatos na agenda, <a href="<?= $BASE_URL ?>create.php">clique aqui para adicionar</a>.</p>
         <?php endif; ?>

dc43d0679e12418037e654ea9ecefb47eac71b72
MarcoUpe <marcojufra@gmail.com>
2024-07-24 23:18:14 -0300

    sec_18_2407

--- 17_agenda/config/process.php
+++ 17_agenda/config/process.php
@@ -5,36 +5,129 @@ session_start();
 include_once("connection.php");
 include_once("url.php");
 
-$id;
+$data = $_POST;
 
-if (!empty($_GET)) {
-    $id = $_GET["id"];
-}
+    // Modificações no banco
+    if (!empty($data)) {
 
-// Retorna o dado de um contato
-if (!empty($id)) {
+        // Criar Contato
+        if($data["type"] === "create") {
 
-    $query = "SELECT * FROM contacts WHERE id = :id";
+            $name = $data["name"];
+            $phone = $data["phone"];
+            $observations = $data["observations"];
 
-    $stmt = $conn->prepare($query);
+            $query = "INSERT INTO contacts (name, phone, observations) VALUES (:name, :phone, :observations)";
 
-    $stmt->bindParam(":id", $id);
+            $stmt = $conn->prepare($query);
 
-    $stmt->execute();
+            $stmt->bindParam(":name", $name);
+            $stmt->bindParam(":phone", $phone);
+            $stmt->bindParam(":observations", $observations);
 
-    $contact = $stmt->fetch();
+            
+        try {
 
-} else {
+            $stmt->execute();
+            $_SESSION["msg"] = "Contato criado com sucesso!";
+ 
+        } catch(PDOException $e) {
+         // erro na conexão
+         $error = $e->getMessage();
+         echo "Erro: $error";
+        }
 
-    // Retorna todos os contatos
-    $contacts = [];
+        } else if($data["type"] === "edit") {
 
-    $query = "SELECT * FROM contacts";
+            $name = $data["name"];
+            $phone = $data["phone"];
+            $observations = $data["observations"];
+            $id = $data["id"];
 
-    $stmt = $conn->prepare($query);
+            $query = "UPDATE contacts SET 
+                     name = :name, phone = :phone, observations = :observations 
+                     WHERE id = :id";
 
-    $stmt->execute();
-    $contacts = $stmt->fetchAll();
-}
+            $stmt = $conn->prepare($query);
+            
+            $stmt->bindParam(":name", $name);
+            $stmt->bindParam(":phone", $phone);
+            $stmt->bindParam(":observations", $observations);
+            $stmt->bindParam("id", $id);
+
+            try {
+
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato atualizado com sucesso!";
+     
+            } catch(PDOException $e) {
+             // erro na conexão
+             $error = $e->getMessage();
+             echo "Erro: $error";
+            }
+
+        } else if($data["type"] === "delete") {
+
+            $id = $data["id"];
+
+            $query = "DELETE FROM contacts WHERE id = :id";
+
+            $stmt = $conn->prepare($query);
+
+            $stmt->bindParam(":id", $id);
+
+            try {
+
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato removido com sucesso!";
+     
+            } catch(PDOException $e) {
+             // erro na conexão
+             $error = $e->getMessage();
+             echo "Erro: $error";
+            }
+
+        }
+
+        // Redirect HOME
+        header("Location:" . $BASE_URL . "../index.php");
+
+    // Selecão de dados
+    } else {
+
+    $id;
+
+    if (!empty($_GET)) {
+        $id = $_GET["id"];
+    }
+
+    // Retorna o dado de um contato
+    if (!empty($id)) {
+
+        $query = "SELECT * FROM contacts WHERE id = :id";
+
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindParam(":id", $id);
+
+        $stmt->execute();
+
+        $contact = $stmt->fetch();
+    } else {
+
+        // Retorna todos os contatos
+        $contacts = [];
+
+        $query = "SELECT * FROM contacts";
+
+        $stmt = $conn->prepare($query);
+
+        $stmt->execute();
+        $contacts = $stmt->fetchAll();
+    }
+    }
+
+    // Fechar Conexão
+    $conn = null;
 
 ?>
--- 17_agenda/create.php
+++ 17_agenda/create.php
@@ -4,15 +4,19 @@
     <div class="container">
         <?php include_once("templates/backbtn.html"); ?>
         <h1 id="main-title">Criar Contato</h1>
-        <form action="<?= $BASE_URL ?>config/process.php" method="POST">
+        <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
             <input type="hidden" name="type" value="create">
             <div class="form-group">
                 <label for="name">Nome do Contato:</label>
-                <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome">
+                <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome" required>
             </div>
             <div class="form-group">
                 <label for="phone">Telefone do Contato:</label>
-                <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o telefone">
+                <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o telefone" required>
+            </div>
+            <div class="form-group">
+                <label for="observations">Observações:</label>
+                <textarea type="text" class="form-control" id="observations" name="observations" placeholder="Insira as Observações" rows="3"></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Cadastrar</button>
         </form>

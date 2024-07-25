644a50a07a8d0932d0ae78cd05d55831825f846f
MarcoUpe <marcojufra@gmail.com>
2024-07-25 17:02:29 -0300

    sec_18_2507

--- 17_agenda/edit.php
+++ 17_agenda/edit.php
@@ -4,7 +4,7 @@
     <div class="container">
         <?php include_once("templates/backbtn.html"); ?>
         <h1 id="main-title">Editar Contato</h1>
-        <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
+        <form id="edit-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
             <input type="hidden" name="type" value="edit">
             <input type="hidden" name="id" value="<?= $contact['id'] ?>">
             <div class="form-group">

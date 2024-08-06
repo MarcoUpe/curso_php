96e6a835922665bac5d9c18dd23fd0260bdac9d4
MarcoUpe <marcojufra@gmail.com>
2024-08-06 16:54:19 -0300

    sec_19_0608

--- 19_moviestar/models/User.php
+++ 19_moviestar/models/User.php
@@ -15,9 +15,13 @@
         public function buildUser($data);
         public function create(User $user, $authUser = false);
         public function update(User $user);
-        public function findByToken($token);
         public function verifyToken($protect = false);
         public function setTokenToSession($token, $redirect = true);
+        public function authenticateUser($email, $password);
+        public function findByEmail($email);
+        public function findById($id);
+        public function findByToken($token);
+        public function changePassword(User $user);
     }
 
 ?>

c7e993c91e07fbfd2d95bdfc96d308919facc05d
MarcoUpe <marcojufra@gmail.com>
2024-08-13 16:58:00 -0300

    sec_19_1308

--- 19_moviestar/auth_process.php
+++ 19_moviestar/auth_process.php
@@ -1,5 +1,10 @@
 <?php
 
-    echo "Oi";
+    require_once("globals.php");
+    require_once("db.php");
+    require_once("models/User.php");
+    require_once("dao/UserDao.php");
+
+    // Resgata o tipo do formulario
 
 ?>

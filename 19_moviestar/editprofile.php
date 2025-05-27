15ad9779ebc35ccdd33a89286372d778acc8b3a6
marcoupe <marcojufra@gmail.com>
2025-05-27 15:23:11 -0300

    comit_2025

--- 19_moviestar/editprofile.php
+++ 19_moviestar/editprofile.php
@@ -11,6 +11,10 @@
 
     $fullName = $user->getFullName($userData);
 
+    if($userData->image == "") {
+        $userData->image = "user.png";
+    }
+
 ?>
     <div id="main-container" class="container-fluid">
         <div class="col-md-12">
@@ -23,6 +27,22 @@
                         <div class="form-group">
                             <label for="name">Nome:</label>
                             <input type="text" class="form-control" id="name" name="name" placeholder="Digite seu nome" value="<?= $userData->name ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="lastname">Sobrenome:</label>
+                            <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Digite seu Sobrenome" value="<?= $userData->lastname ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">E-mail:</label>
+                            <input type="text" readonly class="form-control disable" id="email" name="email" placeholder="Digite seu e-mail" value="<?= $userData->email ?>">
+                        </div>
+                        <input type="submit" class="btn form-btn" value="Alterar">
+                    </div>
+                    <div class="col-md-4">
+                        <div id="profile-image-container" style="background-image: url('<?= $BASE_URL ?>img/user/<?= $userData->image ?>')"></div>
+                        <div class="form-group">
+                            <label for="image">Foto:</label>
+                            <input type="file" class="form-control-file" name="image">
                         </div>
                     </div>
                 </div>
